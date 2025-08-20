1aabb39f4db30e423dd97b2ef9ab9d89117a63b7
minhtc <2053300+minhtc@users.noreply.github.com>
2025-08-20 14:23:50 +0700

    release v3.1

--- Casks/mountmate.rb
+++ Casks/mountmate.rb
@@ -1,6 +1,6 @@
 cask "mountmate" do
-  version "3.0"
-  sha256 "24078d7736f81810085988ce500e4abed38300545451ce4f7b88348959356a2b"
+  version "3.1"
+  sha256 "d28d1bb36d0f76c33f7307fa8a01c2712ab2bd02ed1f8b71a88e77bc80c55280"
 
   url "https://github.com/homielab/mountmate/releases/download/v#{version}/MountMate_#{version}.dmg"
   name "MountMate"
