03a5a77d924ece7563d5a85414702c3d003279f6
minhtc <2053300+minhtc@users.noreply.github.com>
2026-07-26 16:55:35 +0700

    release v5.11

--- Casks/mountmate.rb
+++ Casks/mountmate.rb
@@ -1,6 +1,6 @@
 cask "mountmate" do
-  version "5.10"
-  sha256 "497f0d8a151a311d5a0f65fd0bc43f4c7151aeefb51d7c001fe9a5137a9dddf7"
+  version "5.11"
+  sha256 "3a7cbdca0bd11335f41b8f15ce764acd21316bcbef784a7587b573da19d04bda"
 
   url "https://github.com/homielab/mountmate/releases/download/v#{version}/MountMate_#{version}.dmg",
       verified: "github.com/homielab/mountmate/"
