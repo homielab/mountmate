22cf89d8beab510658931838cecb8ba74996b245
minhtc <2053300+minhtc@users.noreply.github.com>
2026-08-09 01:48:09 +0700

    refactor: simplify UpdaterController initialization and consolidate Sparkle integration logic

--- MountMate/App/MountMateApp.swift
+++ MountMate/App/MountMateApp.swift
@@ -12,14 +12,8 @@ struct MountMateApp: App {
   @StateObject private var driveManager = DriveManager.shared
   @StateObject private var launchManager = LaunchAtLoginManager()
   @StateObject private var diskMounter = DiskMounter()
-  @StateObject private var updaterViewModel: UpdaterController
+  @StateObject private var updaterViewModel = UpdaterController()
   @StateObject private var hotkeyManager = HotkeyManager.shared
-
-  init() {
-    let updater = SPUStandardUpdaterController(
-      startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
-    _updaterViewModel = StateObject(wrappedValue: UpdaterController(updater: updater.updater))
-  }
 
   var body: some Scene {
     MenuBarExtra {
--- MountMate/Managers/UpdaterController.swift
+++ MountMate/Managers/UpdaterController.swift
@@ -4,12 +4,21 @@ import Foundation
 import Sparkle
 import SwiftUI
 
-final class UpdaterController: NSObject, ObservableObject {
-  private let updater: SPUUpdater
+final class UpdaterController: NSObject, ObservableObject, SPUStandardUserDriverDelegate {
+  private var updaterController: SPUStandardUpdaterController!
 
-  init(updater: SPUUpdater) {
-    self.updater = updater
+  override init() {
     super.init()
+    self.updaterController = SPUStandardUpdaterController(
+      startingUpdater: true, updaterDelegate: nil, userDriverDelegate: self)
+  }
+
+  var supportsGentleScheduledUpdateReminders: Bool {
+    true
+  }
+
+  var updater: SPUUpdater {
+    updaterController.updater
   }
 
   @objc func checkForUpdates() {
